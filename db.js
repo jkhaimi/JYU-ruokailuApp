e54bf9a95f4baf1c6aa41f32b44a759aef5d479e
jkhaimi <jesse.haimi@icloud.com>
2025-03-11 19:32:15 +0200

    v1

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 require('dotenv').config(); 
 
 const client = new Client({
-  host: process.env.DB_HOST, // Määritä ympäristömuuttuja
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT, 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
